cab560502bee38afc9039c8b3d459d325dd2c3a7
cjackson <cjackson@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-19 16:48:56 +0000

    Webpage tweaks
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/msm@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -45,21 +45,21 @@ echo $contents; } ?>
 
 <h1>msm: Multi-state models.</h1>
 
-<p> This is the development site for the "msm" R package for continuous-time multi-state modelling. </p>
+<p> This is the development site for the <b><tt>msm</tt></b> R package for continuous-time multi-state modelling. </p>
 
-<p>For more information about the "msm" package and multi-state modelling, please read the paper 
+<p>For more information about the <b><tt>msm</tt></b> package and multi-state modelling, please read the paper 
 <a href="http://www.jstatsoft.org/v38/i08/">Multi-State Models for Panel Data: The msm Package for R</a>
 (Jackson, 2011) from Journal of Statistical Software. </p>
+
+<p>Experienced R and C developers with an interest in multi-state models are welcome to volunteer to contribute to this package.   See the <a href="https://r-forge.r-project.org/scm/viewvc.php/pkg/TODO?view=markup&root=msm">TODO list</a> for some ideas.  The R-Forge <strong>project summary page</strong> can be found <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. 
 
 <h2>Downloads</h2>
 
 <p>The current stable version can be found on <a href="http://CRAN.R-project.org/package=msm">CRAN</a>.</p>
 
-<p>The latest development version can usually be found here on <a href="http://r-forge.r-project.org/R/?group_id=1586">R-Forge</a>.</p>
+<p>The latest development version can usually be found here on <a href="http://r-forge.r-project.org/R/?group_id=1586">R-Forge</a>.  For changes in the development version see the <a href="https://r-forge.r-project.org/scm/viewvc.php/pkg/inst/NEWS?view=markup&root=msm">NEWS</a> or <a href="https://r-forge.r-project.org/scm/viewvc.php/pkg/ChangeLog?view=markup&root=msm">ChangeLog</a> files in the package.</p>
 
-<p>msm is an add-on package for the <a href="http://www.r-project.org/">R</a> statistical software.  Please see the <a href="http://cran.r-project.org/doc/manuals/R-admin.html#Installing-packages">R Installation and Administration Manual</a> for information about installing R packages.</p>
-
-<p>Experienced R and C developers with an interest in multi-state models are welcome to volunteer to contribute to this package.   See the <a href="https://r-forge.r-project.org/scm/viewvc.php/pkg/TODO?view=markup&root=msm">TODO list</a> for some ideas.  The R-Forge <strong>project summary page</strong> can be found <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. 
+<p><b><tt>msm</tt></b> is an add-on package for the <a href="http://www.r-project.org/">R</a> statistical software.  Please see the <a href="http://cran.r-project.org/doc/manuals/R-admin.html#Installing-packages">R Installation and Administration Manual</a> for information about installing R packages.</p>
 
 <hr>
 <address></address>
